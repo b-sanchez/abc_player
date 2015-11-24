c39db5304fa27dd6fa4e538e225ce8d0136b856c
Pranam <pranam@mit.edu>
2015-11-24 14:12:14 -0500

    Added skeleton code for tests for Piece1 and Piece2 from main method (Pranam w/ input from Brandon and Eric)

--- test/abc/sound/SequencePlayerTest.java
+++ test/abc/sound/SequencePlayerTest.java
@@ -2,6 +2,9 @@ package abc.sound;
 
 import static org.junit.Assert.*;
 
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+
 import org.junit.Test;
 
 public class SequencePlayerTest {
@@ -9,9 +12,93 @@ public class SequencePlayerTest {
     // TODO: warmup #2
     
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testPiece1() {
+        try {
+            SequencePlayer player = new SequencePlayer(120, 2);
+
+            player.addNote(new Pitch('C').toMidiNote(), 0, 1);
+            player.addNote(new Pitch('D').toMidiNote(), 1, 1);
+            player.addNote(new Pitch('E').toMidiNote(), 2, 1);
+            player.addNote(new Pitch('F').toMidiNote(), 3, 1);
+            player.addNote(new Pitch('G').toMidiNote(), 4, 1);
+            player.addNote(new Pitch('A').toMidiNote(), 5, 1);
+            player.addNote(new Pitch('B').toMidiNote(), 6, 1);
+            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 7, 1);
+            player.addNote(new Pitch('B').toMidiNote(), 8, 1);
+            player.addNote(new Pitch('A').toMidiNote(), 9, 1);
+            player.addNote(new Pitch('G').toMidiNote(), 10, 1);
+            player.addNote(new Pitch('F').toMidiNote(), 11, 1);
+            player.addNote(new Pitch('E').toMidiNote(), 12, 1);
+            player.addNote(new Pitch('D').toMidiNote(), 13, 1);
+            player.addNote(new Pitch('C').toMidiNote(), 14, 1);
+
+            System.out.println(player);
+
+            // play!
+            player.play();
+
+            /*
+             * Note: A possible weird behavior of the Java sequencer: Even if the
+             * sequencer has finished playing all of the scheduled notes and is
+             * manually closed, the program may not terminate. This is likely
+             * due to daemon threads that are spawned when the sequencer is
+             * opened but keep on running even after the sequencer is killed. In
+             * this case, you need to explicitly exit the program with
+             * System.exit(0).
+             */
+            // System.exit(0);
+
+        } catch (MidiUnavailableException mue) {
+            mue.printStackTrace();
+        } catch (InvalidMidiDataException imde) {
+            imde.printStackTrace();
+        }
     }
     
+    
+    @Test
+    public void testPiece2() {
+        try {
+            SequencePlayer player = new SequencePlayer(120, 2);
+
+            player.addNote(new Pitch('C').toMidiNote(), 0, 1);
+            player.addNote(new Pitch('D').toMidiNote(), 1, 1);
+            player.addNote(new Pitch('E').toMidiNote(), 2, 1);
+            player.addNote(new Pitch('F').toMidiNote(), 3, 1);
+            player.addNote(new Pitch('G').toMidiNote(), 4, 1);
+            player.addNote(new Pitch('A').toMidiNote(), 5, 1);
+            player.addNote(new Pitch('B').toMidiNote(), 6, 1);
+            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 7, 1);
+            player.addNote(new Pitch('B').toMidiNote(), 8, 1);
+            player.addNote(new Pitch('A').toMidiNote(), 9, 1);
+            player.addNote(new Pitch('G').toMidiNote(), 10, 1);
+            player.addNote(new Pitch('F').toMidiNote(), 11, 1);
+            player.addNote(new Pitch('E').toMidiNote(), 12, 1);
+            player.addNote(new Pitch('D').toMidiNote(), 13, 1);
+            player.addNote(new Pitch('C').toMidiNote(), 14, 1);
+
+            System.out.println(player);
+
+            // play!
+            player.play();
+
+            /*
+             * Note: A possible weird behavior of the Java sequencer: Even if the
+             * sequencer has finished playing all of the scheduled notes and is
+             * manually closed, the program may not terminate. This is likely
+             * due to daemon threads that are spawned when the sequencer is
+             * opened but keep on running even after the sequencer is killed. In
+             * this case, you need to explicitly exit the program with
+             * System.exit(0).
+             */
+            // System.exit(0);
+
+        } catch (MidiUnavailableException mue) {
+            mue.printStackTrace();
+        } catch (InvalidMidiDataException imde) {
+            imde.printStackTrace();
+        }
+    }
+
 
 }
