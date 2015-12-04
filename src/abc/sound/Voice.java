07009b98fb26ce4080f7d0ca1cabca1f21bc7dc4
Pranam <pranam@mit.edu>
2015-12-03 22:41:40 -0500

    Moved Music to Voice and VoiceTest

---
+++ src/abc/sound/Voice.java
@@ -0,0 +1,211 @@
+package abc.sound;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import abc.parser.HeadingGrammarLexer;
+import abc.parser.HeadingGrammarParser;
+
+public class Voice {
+    
+
+    
+    private final List<Single> singles;
+
+
+
+    //Rep Invariant:
+    //-this.getDuration() > 0
+    //Abstraction Function AF(value):
+    //-represents a voice that contains singles (Rests, Notes, Chords) to be played
+    //-infoMap maps the header information of an abc file 
+    //Safety from Rep Exposure:
+    //-singles is List of an immutable type Single, and is private and final (not passed between classes)
+    
+    /**
+     * Parse a Voice as a list of Singles.
+     * @param input expression to parse, as defined in the PS3 handout.
+     * @return expression AST for the input
+     * @throws IOException 
+     * @throws IllegalArgumentException if the expression is invalid
+     */
+  
+    public static List<Single> parseSingles(File file) throws IOException {
+        try {
+            String input = "";
+            String input2 = "";
+            boolean stop = false;
+            for(String line: Files.readAllLines(file.toPath())) {
+                if(stop) {
+                    input2+=line+'\r'+'\n';
+                }
+                else {
+                    input+= line+'\r'+'\n';
+                    if(line.charAt(0)=='K') {
+                        stop=true;
+                    }
+                }
+            }
+            System.out.println(input2);
+            CharStream stream2 = new ANTLRInputStream(input2);
+            HeadingGrammarLexer lexer2 = new HeadingGrammarLexer(stream2);
+            TokenStream tokens2 = new CommonTokenStream(lexer2);
+            HeadingGrammarParser parser2 = new HeadingGrammarParser(tokens2);
+            lexer2.reportErrorsAsExceptions();
+            parser2.reportErrorsAsExceptions();
+            ParseTree tree2 = parser2.root();
+            GetNoteInfo infoGetter2 = new GetNoteInfo();
+            new ParseTreeWalker().walk(infoGetter2, tree2);
+            return infoGetter2.getSingles();
+        } catch(RuntimeException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+   
+    /**
+     * Constructor for Voice object
+     * @param measures: List of Single objects to be played in the Voice
+     */
+    public Voice(List<Single> singles){
+        this.singles = singles;
+    }
+    
+    public Voice(File file) throws IOException {
+        this.singles = Voice.parseSingles(file);
+    }
+    
+    /**
+     * Returns the time duration of the voice to be played
+     * @return int duration of piece
+     */
+    public int getDuration() {
+        int duration = 0;
+        for (Single single: this.singles){
+            duration += single.getDuration();
+        }
+        return duration;
+    }
+    
+    /**
+     * Returns the list of singles to be played in the measure
+     * @return List of measures in the Music object
+     */
+    public List<Single> getSingles(){
+        checkRep();
+        return Collections.unmodifiableList(this.singles);
+    }
+    
+    /**
+     * Transposes the singles (chord, note, or rest) in a voice up by a certain number of semitones
+     * @param semitonesUp: integer number of semitones to transpose each single up 
+     * @return the Voice whose singles are transposed up by given number of semitones
+     */
+    public Voice transpose(int semitonesUp) {
+        checkRep();
+        List<Single> transposedList = new ArrayList<Single>();
+        for(Single single: singles){
+            transposedList.add(single.transpose(semitonesUp));
+        }
+        checkRep();
+        return new Voice(transposedList);
+    }
+    
+    /**
+     * Returns the voice as a String representation of the measures within it
+     * @return String representation of Voice object
+     */
+    @Override
+    public String toString() {
+        checkRep();
+        StringBuilder piece = new StringBuilder();
+        for(Single single: singles){
+            piece.append(single.toString());
+        }
+        piece.append("|]");
+        return piece.toString();
+    }
+    
+    /**
+     * Returns integer hashCode for the piece of Music according to the Object contract
+     * @return int hashCode for the piece of Music (two equivalent pieces of Music have the same hashcode)
+     */
+    @Override
+    public int hashCode() {
+        return 17;
+    }
+    
+    /**
+     * Determines equality of two Music  objects by checking for structural equality
+     * @param that: Object to compare this Music with
+     * @return true if two Music objects are identical 
+     */
+    public boolean equals(Object obj) {
+        if(obj instanceof Voice){
+            Voice that = (Voice) obj;
+            //Have to have same number of measures
+            if(that.getSingles().size() != that.getSingles().size()){
+                return false;
+            }
+            //Check for both order and that all measures inside inside are equal
+            for(int i = 0; i < that.getSingles().size(); i++){
+                if(!(this.getSingles().get(i).equals(that.getSingles().get(i)))){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Assert the Rep Invariant
+     */
+    private void checkRep(){
+        assert this.getDuration() > 0;
+    }
+    
+    /**
+     * Plays the Music piece.
+     * addNote(base, tick, duration) schedules a note with pitch value 'base'
+     * starting at 'tick' to be played for 'duration' number of ticks. For example,
+     * addNote(new Pitch('C').toMidiNote(), 10, 1) plays the middle C at
+     * time step 10 for half the duration of a beat.
+     */
+    public void play(){
+        try {
+            SequencePlayer player = new SequencePlayer(140, 12);
+    
+        System.out.println(player);
+
+        // play!
+        player.play();
+        
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        } catch (MidiUnavailableException mue) {
+            mue.printStackTrace();
+        } catch (InvalidMidiDataException imde) {
+            imde.printStackTrace();
+        }
+    }
+}
---
+++ test/abc/sound/VoiceTest.java
@@ -0,0 +1,185 @@
+package abc.sound;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class VoiceTest {
+    
+    //TESTING STRATEGY:
+    //Methods to test: getSingles, getDuration, transpose, toString
+    
+    //getSingles():
+    //-1, 2+ Singles
+    
+    //getDuration():
+    //-1, 2+ Singles
+    //-(duration must be > 0 as part of the Rep Invariant)
+    
+    //transpose():
+    //-Within 0, 1, 2+ octaves
+    //-semitonesUp = negative/positive
+    
+    //toString():
+    //-1, 2+ Singles
+    
+    @Test
+    public void testInfoGetter() throws IOException {
+        System.out.println(Voice.parseSingles(new File("sample_abc/piece2.abc")));
+    }
+    
+    @Test
+    public void testGetSinglesMultiple(){
+        List<Single> noteList = new ArrayList<Single>();
+        Single note1 = new Note(new Pitch('C'), 12);
+        Single note2 = new Note(new Pitch('E'), 12);
+        Single note3 = new Note(new Pitch('G'), 12);
+        noteList.add(note1);
+        noteList.add(note2);
+        noteList.add(note3);
+        Voice voice = new Voice(noteList);
+        assertEquals(noteList, voice.getSingles());
+    }
+    
+    @Test
+    public void testGetSinglesOne(){
+        List<Single> noteList = new ArrayList<Single>();
+        Single note1 = new Note(new Pitch('C'), 12);
+        noteList.add(note1);
+        Voice voice = new Voice(noteList);
+        assertEquals(noteList, voice.getSingles());
+    }
+    
+    @Test
+    public void testGetDurationSinglesMultiple(){
+        List<Single> noteList = new ArrayList<Single>();
+        Single note1 = new Note(new Pitch('C'), 12);
+        Single note2 = new Note(new Pitch('E'), 12);
+        Single note3 = new Note(new Pitch('G'), 12);
+        noteList.add(note1);
+        noteList.add(note2);
+        noteList.add(note3);
+        Voice voice = new Voice(noteList);
+        assertEquals(36, voice.getDuration());
+    }
+    
+    @Test
+    public void testGetDurationSinglesOne(){
+        List<Single> noteList = new ArrayList<Single>();
+        Single note1 = new Note(new Pitch('C'), 12);
+        noteList.add(note1);
+        Voice voice = new Voice(noteList);
+        assertEquals(12, voice.getDuration());
+    }
+    
+    @Test
+    public void testTransposeUpWithinOne(){
+        List<Single> noteList = new ArrayList<Single>();
+        Single note1 = new Note(new Pitch('C'), 12);
+        Single note2 = new Note(new Pitch('D'), 12);
+        Single note3 = new Note(new Pitch('G'), 12);
+        noteList.add(note1);
+        noteList.add(note2);
+        noteList.add(note3);
+        List<Single> noteList2 = new ArrayList<Single>();
+        Single note4 = new Note(new Pitch('D'), 12);
+        Single note5 = new Note(new Pitch('E'), 12);
+        Single note6 = new Note(new Pitch('A'), 12);
+        noteList2.add(note4);
+        noteList2.add(note5);
+        noteList2.add(note6);
+        Voice voice = new Voice(noteList);
+        Voice voice2 = new Voice(noteList2);
+        assertEquals(voice2, voice.transpose(2));
+    }
+    
+    @Test
+    public void testTransposeDownWithinOne(){
+        List<Single> noteList = new ArrayList<Single>();
+        Single note1 = new Note(new Pitch('D'), 12);
+        Single note2 = new Note(new Pitch('E'), 12);
+        Single note3 = new Note(new Pitch('A'), 12);
+        noteList.add(note1);
+        noteList.add(note2);
+        noteList.add(note3);
+        List<Single> noteList2 = new ArrayList<Single>();
+        Single note4 = new Note(new Pitch('C'), 12);
+        Single note5 = new Note(new Pitch('D'), 12);
+        Single note6 = new Note(new Pitch('G'), 12);
+        noteList2.add(note4);
+        noteList2.add(note5);
+        noteList2.add(note6);
+        Voice voice = new Voice(noteList);
+        Voice voice2 = new Voice(noteList2);
+        assertEquals(voice2, voice.transpose(-2));
+    }
+    
+    @Test
+    public void testTransposeUpZero(){
+        List<Single> noteList = new ArrayList<Single>();
+        Single note1 = new Note(new Pitch('D'), 12);
+        Single note2 = new Note(new Pitch('E'), 12);
+        Single note3 = new Note(new Pitch('A'), 12);
+        noteList.add(note1);
+        noteList.add(note2);
+        noteList.add(note3);
+        List<Single> noteList2 = new ArrayList<Single>();
+        Single note4 = new Note(new Pitch('D'), 12);
+        Single note5 = new Note(new Pitch('E'), 12);
+        Single note6 = new Note(new Pitch('A'), 12);
+        noteList2.add(note4);
+        noteList2.add(note5);
+        noteList2.add(note6);
+        Voice voice = new Voice(noteList);
+        Voice voice2 = new Voice(noteList2);
+        assertEquals(voice2, voice.transpose(0));
+    }
+    
+    @Test
+    public void testTransposeUpWithinTwo(){
+        List<Single> noteList = new ArrayList<Single>();
+        Single note1 = new Note(new Pitch('C'), 12);
+        Single note2 = new Note(new Pitch('D'), 12);
+        Single note3 = new Note(new Pitch('G'), 12);
+        noteList.add(note1);
+        noteList.add(note2);
+        noteList.add(note3);
+        List<Single> noteList2 = new ArrayList<Single>();
+        Single note4 = new Note(new Pitch('D'), 12);
+        Single note5 = new Note(new Pitch('E'), 12);
+        Single note6 = new Note(new Pitch('A'), 12);
+        noteList2.add(note4.transpose(Pitch.OCTAVE));
+        noteList2.add(note5.transpose(Pitch.OCTAVE));
+        noteList2.add(note6.transpose(Pitch.OCTAVE));
+        Voice voice = new Voice(noteList);
+        Voice voice2 = new Voice(noteList2);
+        assertEquals(voice2, voice.transpose(14));
+    }
+    
+    @Test
+    public void testTransposeDownWithinTwo(){
+        List<Single> noteList = new ArrayList<Single>();
+        Single note1 = new Note(new Pitch('D'), 12);
+        Single note2 = new Note(new Pitch('E'), 12);
+        Single note3 = new Note(new Pitch('A'), 12);
+        noteList.add(note1);
+        noteList.add(note2);
+        noteList.add(note3);
+        List<Single> noteList2 = new ArrayList<Single>();
+        Single note4 = new Note(new Pitch('C'), 12);
+        Single note5 = new Note(new Pitch('D'), 12);
+        Single note6 = new Note(new Pitch('G'), 12);
+        noteList2.add(note4.transpose(-Pitch.OCTAVE));
+        noteList2.add(note5.transpose(-Pitch.OCTAVE));
+        noteList2.add(note6.transpose(-Pitch.OCTAVE));
+        Voice voice = new Voice(noteList);
+        Voice voice2 = new Voice(noteList2);
+        assertEquals(voice2, voice.transpose(-14));
+    }
+    
+}
